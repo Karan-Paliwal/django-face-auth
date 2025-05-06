c099893df71220077689ddfb8285c12c7d856321
Karan-Paliwal <paliwalkaran06@gmail.com>
2025-05-06 10:18:16 +0530

    settings.py

--- website/settings.py
+++ website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ibyf@-@rpe1@)%i)0*r_pwq%%sapal&0*g6ru81t=z((vdej&k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['AIRZ', 'localhost','127.0.0.1']
+ALLOWED_HOSTS = ['AIRZ', 'localhost','127.0.0.1','https://django-face-auth.onrender.com']
 
 
 # Application definition
